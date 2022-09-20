e44b3c6e57796760916e8f81c234ba90c264743d
Satish Patil <satish.patil7017@gmail.com>
2022-09-20 09:36:27 +0530

    [Satish] Added creating table

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,2 +1,15 @@
 create database AddressBook_serviceDB;
 use AddressBook_serviceDB;
+
+--UC2 add Addressbook table--
+create table AddressBook(
+FirstName varchar(100),
+LastName varchar(100),
+Address varchar(100),
+City varchar(100),
+State varchar (100),
+Zip bigint,
+PhoneNumber bigint,
+Email varchar(100)
+);
+select * from AddressBook;
